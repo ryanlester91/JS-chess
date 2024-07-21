e83f24a4639ca96179909b40c6f50ba5ecdc1198
ryanlester91 <lestertr@dukes.jmu.edu>
2024-07-21 18:20:40 -0400

    completed cases for pawn and knight moves

--- app.js
+++ app.js
@@ -109,10 +109,28 @@ function checkIfValid(target) {
         case 'pawn' :
             const starterRow = [8, 9, 10, 11, 12, 13, 14, 15]
             if (
-                starterRow.includes(startId) && startId + width * 2 === targetId
+                starterRow.includes(startId) && startId + width * 2 === targetId  ||
+                startId + width === targetId || startId + width - 1 === targetId && document.querySelector(`[square-id=${startId + width - 1}]`).firstChild ||
+                startId + width + 1 === targetId && document.querySelector(`[square-id=${startId + width + 1}]`).firstChild 
+
             ) {
                 return true
             }
+            break;
+            
+            case 'knight':
+                if (
+                    startId + width * 2 + 1 === targetId ||
+                    startId + width * 2 - 1 === targetId ||
+                    startId + width - 2 === targetId ||
+                    startId + width + 2 === targetId ||
+                    startId - width * 2 + 1 === targetId ||
+                    startId - width * 2 - 1 === targetId ||
+                    startId - width - 2 === targetId ||
+                    startId - width + 2 === targetId
+                ) {
+                    return true
+                }
     }
 }
 
