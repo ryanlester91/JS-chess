7e12d61503605b2e97c7d2986c939826a14d9aa9
ryanlester91 <lestertr@dukes.jmu.edu>
2024-07-21 17:48:56 -0400

    target runs thru checkIfValid function

--- app.js
+++ app.js
@@ -26,16 +26,16 @@ function createBoard() {
         //square.classList.add('beige')
         const row = Math.floor( (63 - i) / 8) + 1
         if ( row % 2 === 0) {
-            square.classList.add(i % 2 === 0 ? "beige" : "brown")
+            square.classList.add(i % 2 === 0 ? 'beige' : 'brown')
         } else {
-            square.classList.add(i % 2 === 0 ? "brown" : "beige")
+            square.classList.add(i % 2 === 0 ? 'brown' : 'beige')
         }
 
         if ( i <=15 ) {
-            square.firstChild.firstChild.classList.add("black");
+            square.firstChild.firstChild.classList.add('black');
         } 
         if ( i >=48 ) {
-            square.firstChild.firstChild.classList.add("white");
+            square.firstChild.firstChild.classList.add('white');
         }
         gameBoard.append(square)
     })
@@ -68,6 +68,7 @@ function dragDrop (e){
     e.stopPropagation()
     const correctGo = draggedElement.firstChild.classList.contains(playerGo)
     const taken = e.target.classList.contains('piece')
+    const valid = checkIfValid(e.target)
     const opponentGo = playerGo === 'white' ? 'black' : 'white'
     const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
     changePlayer()
@@ -80,6 +81,38 @@ function dragDrop (e){
             changePlayer()
             return
         }
+
+        //then check this
+        if (taken && !takenByOpponent) {
+            infoDisplay.textContent = "You cannot go here!"
+            setTimeout(() => infoDisplay.textContent = "", 2000)
+            return
+        }
+
+        if (valid) {
+            e.target.parentNode.append(draggedElement)
+            changePlayer()
+            return
+        }
+    }
+}
+
+function checkIfValid(target) {
+    const targetId = Number(target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'))
+    const startId = Number(startPositionId)
+    const piece = draggedElement.id
+    console.log(targetId)
+    console.log(startId)
+    console.log(piece)
+
+    switch(piece) {
+        case 'pawn' :
+            const starterRow = [8, 9, 10, 11, 12, 13, 14, 15]
+            if (
+                starterRow.includes(startId) && startId + width * 2 === targetId
+            ) {
+                return true
+            }
     }
 }
 
